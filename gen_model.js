c640a2c096f6a924fe512e5431d29391684aed57
linzf <mmzp@qq.com>
2020-04-01 15:36:22 +0800

    update: 代码重构

--- gen_model.js
+++ gen_model.js
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
-const modelGenerator = require('./lib/model_generator');
+const modelParser = require('./lib/model_parser');
 const { scanDir } = require('./lib/utils');
+const ejs = require('ejs');
+const path = require('path');
 const argvs = require('yargs').argv;
 
 const sqlFile = argvs.sql;
@@ -17,7 +19,52 @@ if (!modelPath) {
     return;
 }
 
-(async ()=>{
+const template =
+`import { table, primaryKey } from '../luna';
+import { Model, FindOptions } from '../db';
+
+<% if (tableComment) { %>// <%= tableComment %><% } %>
+@table('<%= tableName %>')
+export class <%= modelName %> extends Model {
+    <% for(const column of columns) { %><% if (column.isPK) { %>@primaryKey<% } %>
+    <%= column.name %>?: <%= column.type %> = undefined;<% } %>
+
+    static async findOne(id: number | string): Promise<<%= modelName %> | undefined>;
+    static async findOne(options: FindOptions): Promise<<%= modelName %> | undefined>;
+    static async findOne(p1: any) {
+        return Model._findOne(<%= modelName %>, p1);
+    }
+    static async findAll(idArr: Array<number | string>): Promise<<%= modelName %>[]>;
+    static async findAll(options: FindOptions): Promise<<%= modelName %>[]>;
+    static async findAll(p1: any) {
+        return Model._findAll(<%= modelName %>, p1);
+    }
+    static async fetch(sql: string, params?: any[]): Promise<<%= modelName %> | undefined> {
+        return Model._fetch(<%= modelName %>, sql, params);
+    }
+    static async fetchAll(sql: string, params?: any[]): Promise<<%= modelName %>[]> {
+        return Model._fetchAll(<%= modelName %>, sql, params);
+    }
+    static async insert(info: <%= modelName %>): Promise<<%= modelName %>> {
+        return Model._insert(info);
+    }
+    static async update(id: number | string, info: <%= modelName %>): Promise<number>;
+    static async update(options: FindOptions, info: <%= modelName %>): Promise<number>;
+    static async update(p1: any, info: <%= modelName %>): Promise<number> {
+        return Model._update(p1, info);
+    }
+    static async delete(id: number | string, info: <%= modelName %>): Promise<number>;
+    static async delete(options: FindOptions, info: <%= modelName %>): Promise<number>;
+    static async delete(p1: any, info: <%= modelName %>): Promise<number> {
+        return Model._delete(p1, info);
+    }
+    static async exec(sql: string, params?: any[]): Promise<number> {
+        return Model._exec(sql, params);
+    }
+}
+`;
+
+(async () => {
     const oldFiles = await scanDir(modelPath);
     for (const filename of oldFiles) {
         if (filename.slice(-7) === '.gen.ts') {
@@ -27,22 +74,33 @@ if (!modelPath) {
 
     const sqlContent = fs.readFileSync(sqlFile).toString('utf8');
     const sqlContentArr = sqlContent.split('\n\n');
-    for (let content of sqlContentArr) {
-        if (content.slice(0, 12).toUpperCase() === 'CREATE TABLE') {
-            await modelGenerator.generate(content, modelPath);
-        }
-    }
-
-    const files = await scanDir(modelPath);
+    const modelSchemeArr = [];
     let exportIndexContent = '';
     let modelCount = 0;
-    for (const filename of files) {
-        if (filename.slice(-7) === '.gen.ts') {
-            exportIndexContent += `export * from './${filename.slice(0, -3)}';\n`;
-            modelCount++;
+    for (let content of sqlContentArr) {
+        if (content.slice(0, 12).toUpperCase() === 'CREATE TABLE') {
+            const models = await modelParser.parse(content, modelPath);
+            modelSchemeArr.push(...models);
         }
     }
+    for (const modelScheme of modelSchemeArr) {
+        const context = await ejs.render(template, {
+            tableName: modelScheme.tableName,
+            modelName: modelScheme.modelName,
+            columns: modelScheme.columns,
+            tableComment: modelScheme.tableComment,
+        });
+        const filename = pluralize.singular(modelScheme.tableName) + '.gen.ts';
+        fs.writeFileSync(path.resolve(modelPath, `${filename}`), context);
+        exportIndexContent += `export { ${modelScheme.modelName} } from './${filename.slice(0, -3)}';\n`;
+        modelCount++;
+    }
+
+    if (!exportIndexContent) {
+        exportIndexContent = 'export {};\n';
+    }
     fs.writeFileSync(modelPath + '/generated.ts', exportIndexContent);
+
     if (!fs.existsSync(modelPath + '/custom.ts')) {
         fs.writeFileSync(modelPath + '/custom.ts', 'export {};\n');
     }
